50f95dd631dcd52f11b18c23cae281917a6ca1fe
Virom067 <roman.magnum@gmail.com>
2022-07-06 23:56:59 +0500

    changed the yadisk on requests

--- task_2.py
+++ task_2.py
@@ -1,18 +1,33 @@
-import yadisk
+import requests
 import os
 
 class YaUploader:
-    def __init__(self, token: str):
-        self.y = yadisk.YaDisk(token=token)
+    def __init__(self, token):
+        self.token = token
+        self.url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        self.headers = {'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': f'OAuth {self.token}'}
 
-    def upload_for_disk(self, file_path: str):
-        file_name = os.path.basename(file_path)
-        self.y.upload(file, file_name)
+    def upload(self, path_to_file):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        upload_url = self.url + '/upload'
+        response = requests.get(upload_url,
+                               headers=self.headers,
+                               params={'path': os.path.basename(path_to_file),
+                                       'overwrite': 'replace'})
+        with open(path_to_file, 'rb') as f:
+            try:
+                requests.put(response.json()['href'], files={'file': f})
+                print('OK!')
+            except KeyError:
+                print(response)
+
 
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    file = ...
+    path_to_file = ...
     token = ...
     uploader = YaUploader(token)
-    uploader.upload_for_disk(file)
+    result = uploader.upload(path_to_file)
